45af8d48ffd0206ca41badb92bdc368bcf2861f2
Wai Lin <lwai661467@gmail.com>
2023-10-03 20:27:22 +0630

    init Project

---
+++ src/pages/backoffice/menu-category/MenuCategory.tsx
@@ -0,0 +1,5 @@
+const MenuCategory = () => {
+  return <h1>Welcome to menu category page</h1>;
+};
+
+export default MenuCategory;
---
+++ src/pages/backoffice/menu/Menu.tsx
@@ -0,0 +1,5 @@
+function Menu() {
+  return <h1>Welcome to menu page</h1>;
+}
+
+export default Menu;
